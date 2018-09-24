011fb8ed3e9b3d24262f93fcebe8a40da9c42036
CrustyBarnacle <CrustyBarnaclee@users.noreply.github.com>
2018-09-24 10:55:07 -0700

    Adds 'The menu' test suite function.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -53,6 +53,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -81,4 +82,9 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         
+         
+    });
+    
+         
 }());
